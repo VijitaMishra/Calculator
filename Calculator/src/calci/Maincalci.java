4f7fd0de4c9b52520a16862e7d9cb6cc860eeb43
ishani mishra <ishani mishra@LAPTOP-70DD00S2>
2026-02-24 07:34:37 -0500

    commit calci

---
+++ Calculator/src/calci/Maincalci.java
@@ -0,0 +1,14 @@
+package calci;
+
+public class Maincalci {
+	
+	public static void main(String[] args){
+		Testcalci calci = new Testcalci();
+		calci.add();
+		calci.minus();
+		calci.multiply();
+		calci.divide();
+		
+	}
+
+}
---
+++ Calculator/src/calci/Testcalci.java
@@ -0,0 +1,38 @@
+package calci;
+
+public class Testcalci {
+	public void add(){
+	
+		int num1=10;
+		int num2=5;
+		int sum= num1 + num2;
+		
+		System.out.println("Total:" + sum);
+	}
+					
+	public void minus(){
+		int num1=10;
+		int num2=5;
+		int minus= num1 - num2;
+		
+		System.out.println("Total:" + minus);
+	}
+		
+	public void multiply(){
+		int num1=10;
+		int num2=5;
+		int multiply= num1 * num2;
+		
+		System.out.println("Total:" + multiply);
+	}
+		
+	public void divide(){
+		
+		int num1=10;
+		int num2=5;
+		int divide= num1 / num2;
+		
+		System.out.println("Total:" + divide);
+	}
+
+}
